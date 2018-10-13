82d45d380e5452f08e70b318d23ffed8183cfc8f
Niks <niks@nikss-macbook-pro.local>
2018-10-13 10:08:08 +0530

    Initial Commit

---
+++ SocialMedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialMedia
+//
+//  Created by Niks on 13/10/18.
+//  Copyright © 2018 Niks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
